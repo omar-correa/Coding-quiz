2b218707cfce2343b8a7e732d473754f4cdca1e4
Omar Correa <omarcorrea378@yahoo.com>
2022-10-18 17:34:23 -0400

    first day push

---
+++ quiz.js
@@ -0,0 +1,19 @@
+var start = document.querySelector("#start")
+var quiz = document.querySelector("#quiz")
+var answerA = document.querySelector("#answerA")
+var answerB = document.querySelector("#answerB")
+var answerC = document.querySelector("#answerC")
+var answerD = document.querySelector("#answerD")
+
+quiz.style.display = "none"
+
+
+function commence() {
+
+    start.style.display = "none"
+    quiz.style.display = "block"
+}
+
+function aChoice() {
+    
+}
