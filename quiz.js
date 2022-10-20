a35b50b3d5c6f9abb973fd40304d4b1bad9dd1a3
Omar Correa <omarcorrea378@yahoo.com>
2022-10-20 09:22:07 -0400

    second day push

--- quiz.js
+++ quiz.js
@@ -1,9 +1,21 @@
-var start = document.querySelector("#start")
-var quiz = document.querySelector("#quiz")
-var answerA = document.querySelector("#answerA")
-var answerB = document.querySelector("#answerB")
-var answerC = document.querySelector("#answerC")
-var answerD = document.querySelector("#answerD")
+var start = document.querySelector("#start");
+var quiz = document.querySelector("#quiz");
+var question = document.querySelector("#question")
+var answerA = document.querySelector("#answerA");
+var answerB = document.querySelector("#answerB");
+var answerC = document.querySelector("#answerC");
+var answerD = document.querySelector("#answerD");
+var specificQuestion = 0;
+var answerChoice = "";
+var score = 0;
+
+var questionList = [
+    "1. Which of the following is not one of the 7 data types in JavaScript?",
+    "2.",
+    "3.",
+    "4.",
+    "5."
+]
 
 quiz.style.display = "none"
 
@@ -12,8 +24,41 @@ function commence() {
 
     start.style.display = "none"
     quiz.style.display = "block"
-}
+    question.innerHTML = questionList[specificQuestion]
 
-function aChoice() {
-    
-}
+    function aChoice() {
+        answerA.style.color = 'red'
+        answerB.style.color = 'black'
+        answerC.style.color = 'black'
+        answerD.style.color = 'black'
+        answerChoice = "A"
+
+    }
+    function bChoice() {
+        answerA.style.color = 'black'
+        answerB.style.color = 'green'
+        answerC.style.color = 'black'
+        answerD.style.color = 'black'
+        answerChoice = "B"
+    }
+    function cChoice() {
+        answerA.style.color = 'black'
+        answerB.style.color = 'black'
+        answerC.style.color = 'purple'
+        answerD.style.color = 'black'
+        answerChoice = "C"
+    }
+    function dChoice() {
+        answerA.style.color = 'black'
+        answerB.style.color = 'black'
+        answerC.style.color = 'black'
+        answerD.style.color = 'blue'
+        answerChoice = "D"
+    }
+
+    function submitQuestion1() {
+        if (answerChoice == "D") {
+            score = score + 1;
+        }
+    }
+}    
